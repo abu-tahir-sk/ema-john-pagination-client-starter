cd468ecc3167519eef71ba1c9c2d81bcd38f9cd7
abu-tahir-sk <abutahir2626@gmail.com>
2025-11-21 15:07:45 -1000

    next and prev

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -26,7 +26,9 @@ const Shop = () => {
   const pages = [...Array(numberOfPages).keys()];
 
   useEffect(() => {
-    fetch("http://localhost:5000/products")
+    fetch(
+      `http://localhost:5000/products?page=${currentPage}&size=${itemsPerPage}`
+    )
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
